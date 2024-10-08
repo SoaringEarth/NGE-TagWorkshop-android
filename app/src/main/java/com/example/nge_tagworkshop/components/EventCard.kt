c03a0eb43010e63c59cd0718fd20bb536ac0adbc
Jonathon Albert <jonathon.albert@digital.mod.uk>
2024-10-08 18:06:21 +0100

    add loading spinners

--- app/src/main/java/com/example/nge_tagworkshop/components/EventCard.kt
+++ app/src/main/java/com/example/nge_tagworkshop/components/EventCard.kt
@@ -1,6 +1,5 @@
 package com.example.nge_tagworkshop.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,10 +8,12 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,20 +24,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import coil.compose.AsyncImage
-import com.example.nge_tagworkshop.R
+import coil.compose.SubcomposeAsyncImage
 import com.example.nge_tagworkshop.api.PexelRepository
 import com.example.nge_tagworkshop.api.Photo
 import com.example.nge_tagworkshop.models.Category
 import com.example.nge_tagworkshop.models.Event
 import kotlinx.coroutines.launch
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Currency
+import java.util.Locale
 
 
 class EventCardViewModel(var event: Event, repository: PexelRepository = PexelRepository()): ViewModel() {
@@ -52,6 +55,24 @@ class EventCardViewModel(var event: Event, repository: PexelRepository = PexelRe
     private suspend fun getEventPhoto(event: Event, repository: PexelRepository) {
         println("Getting photo for event: ${event.title}")
         eventPhoto.value = repository.getPhoto(query = event.title)
+    }
+
+    fun parseCustomUtcString(): String {
+        val parsedDate = ZonedDateTime.parse(event.time) // Parse the UTC string to ZonedDateTime
+        val formatter = DateTimeFormatter.ofPattern("MMM-dd, yyyy") // Define the desired format
+        return parsedDate.format(formatter) // Format the parsed date
+    }
+
+    fun getEventCost(): String {
+        if (event.price == 0) {
+            return "Free"
+        } else {
+            return "${getLocalCurrencySymbol()}${event.price}"
+        }
+    }
+
+    private fun getLocalCurrencySymbol(locale: Locale = Locale.getDefault()): String {
+        return Currency.getInstance(locale).symbol
     }
 }
 
@@ -87,7 +108,7 @@ fun EventCard(
                 maxLines = Int.MAX_VALUE
             )
             Text(
-                text = "${viewModel.event.location} | ${viewModel.event.time}",
+                text = "${viewModel.event.location} | ${viewModel.parseCustomUtcString()}",
                 fontSize = MaterialTheme.typography.bodyMedium.fontSize,
                 fontWeight = FontWeight.Normal,
                 color = MaterialTheme.colorScheme.onSurface
@@ -116,7 +137,7 @@ fun EventCard(
             ) {
                 Text(
                     modifier = Modifier,
-                    text = "${viewModel.event.price}",
+                    text = viewModel.getEventCost(),
                     fontSize = MaterialTheme.typography.bodyMedium.fontSize,
                     fontWeight = FontWeight.Normal,
                     color = MaterialTheme.colorScheme.onSurface,
@@ -133,19 +154,20 @@ fun BannerImage(photo: MutableState<Photo?>) {
         .heightIn(max = 180.dp)
         .padding(bottom = 8.dp)
     if (photo.value == null) {
-        println("Photo is null")
-        Image(
-            painter = painterResource(id = R.drawable.placeholder),
-            contentDescription = "",
-            contentScale = ContentScale.FillWidth,
-            modifier = imageModifier
-        )
+        Box(modifier = Modifier.size(200.dp), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+            )
+        }
     } else {
-        println("Photo is not null")
-        AsyncImage(
+        SubcomposeAsyncImage(
             model = photo.value!!.src.medium, // Use any available size URL from the Photo object
             contentDescription = "Photo by ${photo.value!!.photographer}",
             contentScale = ContentScale.FillWidth,
+            loading = {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            },
             modifier = imageModifier
                 .aspectRatio(1.5f) // Adjust aspect ratio as needed
                 .clip(RoundedCornerShape(16.dp))
